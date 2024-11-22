eb0aa16842e4eaf5383e43e5df717384001f56bf
Your Name <you@example.com>
2024-11-23 01:10:48 +0900

    1231

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react'; // React에서 Component 가져오기
-import logo from './logo.svg'; // 중복 제거, 이미지 경로
 import './App.css'; // CSS 경로
+import Customer from './components/Customer';
 
+const customers =[{
+  'id' :1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name': '홍길동',
+  'birthday':'9611222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id' :2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name': '홍e동',
+  'birthday':'9611222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id' :3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name': '홍q동',
+  'birthday':'9611222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id' :4,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name': '홍q동',
+  'birthday':'9611222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id' :5,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name': '홍q동',
+  'birthday':'9611222',
+  'gender':'남자',
+  'job':'대학생'
+}
+]
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>안녕1231</h2>
+      <div>
+        {customers.map((customer) => (
+          <Customer
+            key={customer.id} // 각 항목에 고유한 key를 제공해야 함
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        ))}
       </div>
     );
   }
